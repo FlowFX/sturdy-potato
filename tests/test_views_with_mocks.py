253ac2c746a836ec56994539dc5564505be4ba30
Florian Posdziech <hallo@flowfx.de>
2017-03-10 10:26:51 -0600

    Document mokey-patch code

--- tests/test_views_with_mocks.py
+++ tests/test_views_with_mocks.py
@@ -10,12 +10,17 @@ def test_detail_view(client):
     """Test the detail view for a Potato object with the Django test client."""
 
     # GIVEN a Potato object
-    potato = PotatoFactory.build()
-    potato.id = 5       # The ID is set when saving. We don't want that.
+    potato = PotatoFactory.build()  # This is not saved to the database
+    potato.id = 5                   # The ID is set when saving. We don't want that.
 
+    """Here we monkey-patch the 'get_object" method of our DetailView.
+    No matter with what kwargs we call the view, it will always use the
+    'potato' object we pass as the 'return_value'. The view's original
+    'get_object' is ignored. This means: no database call!
+    """
     with patch.object(PotatoDetailView, 'get_object', return_value=potato):
         # WHEN calling the DetailView for this object
-        url = reverse('detail', kwargs={'pk': potato.id})
+        url = reverse('detail', kwargs={'pk': 1234})  # pk can be anything
         response = client.get(url)
 
         content = response.content.decode()
@@ -31,6 +36,9 @@ def test_list_view(client):
     # GIVEN a number of potatos
     potatos = PotatoFactory.build_batch(5)
 
+    """Same monkey-patching as above. For the ListView it is the 'get_queryset'
+    method that we want to disable.
+    """
     with patch.object(PotatoListView, 'get_queryset', return_value=potatos):
         # WHEN calling the list view for our potatos
         url = reverse('list')
