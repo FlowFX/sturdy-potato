6ea8f9f2af89ec4d892cbdffa41b9a4c2610d4db
Florian Posdziech <hallo@flowfx.de>
2017-03-10 08:24:01 -0600

    Develop view tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,15 +1,18 @@
 from django.urls import reverse
 
+
 def test_index_view(client):
+    """Test the index view using the Django test client."""
+
     # GIVEN the home page
     url = reverse('index')
 
     # WHEN calling it with the Django test client
     response = client.get(url)
 
-    # THEN it's there
+    # THEN it's there,
     assert response.status_code == 200
-
-    # AND the project title is visible
+    # the project title is visible,
     assert '<h1>Sturdy Potato</h1>' in response.content.decode()
-
+    # and the correct template is used
+    assert 'index.html' in (template.name for template in response.templates)
