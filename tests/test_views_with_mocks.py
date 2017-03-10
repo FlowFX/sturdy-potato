d32719e204fc3867606773b8762ebbdc7478e9cc
Florian Posdziech <hallo@flowfx.de>
2017-03-10 10:08:14 -0600

    Add test with object mock

---
+++ tests/test_views_with_mocks.py
@@ -0,0 +1,27 @@
+from django.urls import reverse
+
+from potatos.factories import PotatoFactory
+from potatos.views import PotatoDetailView
+
+from mock import patch
+
+
+def test_detail_view(client):
+    """Test the detail view for a Potato object with the Django test client."""
+
+    # GIVEN a Potato object
+    potato = PotatoFactory.build()
+    potato.id = 5       # The ID is set when saving. We don't want that.
+
+    with patch.object(PotatoDetailView, 'get_object', return_value=potato):
+        # WHEN calling the DetailView for this object
+        url = reverse('detail', kwargs={'pk': potato.id})
+        response = client.get(url)
+
+        content = response.content.decode()
+        # THEN it shows the potato's ID and it's type
+        assert response.status_code == 200
+        assert str(potato.weight) in content
+        assert potato.variety in content
+
+
