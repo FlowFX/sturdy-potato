2fb8abb77d0a7508d015525b091e56d33c7cc255
Florian Posdziech <hallo@flowfx.de>
2017-04-11 16:25:19 -0500

    Move templates folder out of app

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
