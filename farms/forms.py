bff37b50e46dc2d01399e2b3cbefe40b0bd2eef7
Florian Posdziech <hallo@flowfx.de>
2017-04-11 17:59:07 -0500

    Add the submit button to AddressForm

--- farms/forms.py
+++ farms/forms.py
@@ -19,17 +19,10 @@ class AddressForm(forms.ModelForm):
             'postal_code',
             'city',
         )
-    #
-    # def __init__(self, *args, **kwargs):
-    #     """Initiate form with Crispy Form's FormHelper."""
-    #     super(PotatoForm, self).__init__(*args, **kwargs)
-    #     self.helper = FormHelper()
-    #
-    #     self.helper.add_input(Submit('submit', 'Submit'))
-    #     self.helper.add_input(Submit(
-    #         'cancel',
-    #         'Cancel',
-    #         css_class='btn-danger',
-    #         formnovalidate='formnovalidate',
-    #     )
-    #     )
+
+    def __init__(self, *args, **kwargs):
+        """Initiate form with Crispy Form's FormHelper."""
+        super(AddressForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+
+        self.helper.add_input(Submit('submit', 'Submit'))
