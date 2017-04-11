ff610245463e15ed0a352b9bbf1b9bf81a9b911b
Florian Posdziech <hallo@flowfx.de>
2017-04-11 17:44:23 -0500

    Add test for address create POST view

--- farms/models.py
+++ farms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Address(models.Model):
@@ -7,4 +8,8 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     postal_code = models.CharField(max_length=5)
     city = models.CharField(max_length=255)
+
+    def get_absolute_url(self):
+        """Return URL of the object's detail view."""
+        return reverse('farms:address_detail', args=[str(self.id)])
 
